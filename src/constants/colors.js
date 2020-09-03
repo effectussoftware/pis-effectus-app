b51bdd610b4dbe5cc2de8d75e12dd2c22d65c64e
Agustina Chaer <aguschaer@gmail.com>
2020-09-03 19:18:03 -0300

    fix: some colors where missing their final value

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,10 +2,10 @@ export const PRIMARY = '#2C4781';
 export const SECONDARY = '#4A6CCB';
 
 export const WHITE = '#FFFFFF';
-export const SUPER_LIGHT_GRAY = '#FFFFFF';
-export const LIGHT_GRAY = '#FFFFFF';
-export const GRAY = '#FFFFFF';
-export const DARK_GRAY = '#FFFFFF';
-export const BLACK = '#FFFFFF';
+export const SUPER_LIGHT_GRAY = '#F6F6F6';
+export const LIGHT_GRAY = '#E8E8E8';
+export const GRAY = '#BDBDBD';
+export const DARK_GRAY = '#666666';
+export const BLACK = '#000000';
 
 export const ERROR = '#FF5252';
