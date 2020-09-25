d961ac223a34404119154d5ca21eb4cd964b0142
JoaquinBartaburu <58480913+JoaquinBartaburu@users.noreply.github.com>
2020-09-25 19:31:58 -0300

    Update MainScreen.js

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { object } from 'prop-types';
 import { View } from 'react-native';
 
 import { MAIN_SCREEN } from 'constants/screens';
 import Text from 'components/Text';
 import strings from 'locale';
 import useSession from 'hooks/useSession';
+import SignOut from 'components/SignOut';
 import styles from './MainScreen.styles';
 
-const MainScreen = ({ navigation }) => {
-  navigation.setOptions({ title: strings.MAIN_SCREEN.title });
-
+const MainScreen = () => {
   const {
     user: { email },
   } = useSession();
 
   return (
     <View style={styles.container} testID={MAIN_SCREEN}>
+      <SignOut testID="logout-button" />
       <Text type="H2">{`Hey ${email || ''}, you're logged in!`}</Text>
     </View>
   );
 };
 
-MainScreen.propTypes = {
-  navigation: object.isRequired,
+MainScreen.navigationOptions = {
+  title: strings.MAIN_SCREEN.title,
 };
 
 export default MainScreen;
