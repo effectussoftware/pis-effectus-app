5c1229d0df11fe5d5811502fa246db1a1a77bef3
Oscar Muñoz <osmunogue@gmail.com>
2020-10-08 21:32:05 -0300

    feat: release feed component

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -4,6 +4,7 @@ import { oneOf } from 'prop-types';
 import OneOnOneIcon from 'assets/images/feedIcons/oneOnOne/default.png';
 import PollIcon from 'assets/images/feedIcons/poll/default.png';
 import ExchangeIcon from 'assets/images/feedIcons/exchange/default.png';
+import Release from 'assets/images/feedIcons/release/default.png';
 import Card from 'components/Card';
 import Button from 'components/Button';
 import strings from 'locale';
@@ -13,15 +14,17 @@ import styles from './FeedCard.styles';
 const POLL = 'poll';
 const EXCHANGE = 'exchange';
 const ONE_ON_ONE = 'oneOnOne';
+const RELEASE = 'release';
 const NEWS = 'news';
 
-export const typeShape = oneOf([POLL, EXCHANGE, ONE_ON_ONE, NEWS]);
+export const typeShape = oneOf([POLL, EXCHANGE, ONE_ON_ONE, NEWS, RELEASE]);
 
 const icons = {
   [POLL]: PollIcon,
   [EXCHANGE]: ExchangeIcon,
   [ONE_ON_ONE]: OneOnOneIcon,
   [NEWS]: OneOnOneIcon,
+  [RELEASE]: Release,
 };
 
 const FeedCard = ({ type, ...restProps }) => {
