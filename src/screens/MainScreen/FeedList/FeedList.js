d7b163080227f8e92cefdb93c9529fba5fb0ec35
Agustina Chaer <aguschaer@gmail.com>
2020-10-21 09:08:06 -0300

    fix: add empty state and error alert to feed

--- src/screens/MainScreen/FeedList/FeedList.js
+++ src/screens/MainScreen/FeedList/FeedList.js
@@ -1,14 +1,17 @@
 import React, { useCallback, useEffect } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList, View } from 'react-native';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { useLoading } from '@rootstrap/redux-tools';
+import { LOADING, SUCCESS, useStatus } from '@rootstrap/redux-tools';
 
-import FeedCard from 'components/FeedCard/index';
-
+import strings from 'locale';
 import { getFeed } from 'actions/feedActions';
-import styles from './FeedFlatList.styles';
 
-const FeedFlatList = () => {
+import { Text } from 'components';
+import FeedCard from './FeedCard/index';
+
+import styles from './FeedList.styles';
+
+const FeedList = () => {
   const dispatch = useDispatch();
 
   const handleRefresh = useCallback(() => {
@@ -19,7 +22,13 @@ const FeedFlatList = () => {
     handleRefresh();
   }, [handleRefresh]);
 
-  const loading = useLoading(getFeed);
+  const { status, error } = useStatus(getFeed);
+
+  useEffect(() => {
+    error && Alert.alert(error);
+  }, [error]);
+
+  const loading = status === LOADING;
 
   const { data: feed, endReached } = useSelector(({ feed }) => feed, shallowEqual);
 
@@ -31,6 +40,7 @@ const FeedFlatList = () => {
     <FlatList
       style={styles.flatList}
       data={feed}
+      contentContainerStyle={styles.contentContainer}
       renderItem={({ item }) => <FeedCard {...item} />}
       keyExtractor={item => item.id.toString()}
       onEndReached={handleLoadMore}
@@ -38,8 +48,17 @@ const FeedFlatList = () => {
       initialNumToRender={10}
       onRefresh={handleRefresh}
       refreshing={loading}
+      ListEmptyComponent={
+        status === SUCCESS &&
+        // eslint-disable-next-line react/no-multi-comp
+        (() => (
+          <View style={styles.emptyState}>
+            <Text type="H3">{strings.MAIN_SCREEN.emptyState}</Text>
+          </View>
+        ))
+      }
     />
   );
 };
 
-export default FeedFlatList;
+export default FeedList;
