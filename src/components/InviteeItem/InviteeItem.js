9d438a28e2dd8d554527c1a3c6ca47df15e204a9
JoaquinBartaburu <jobak12@gmail.com>
2020-10-22 19:30:00 -0300

    removed-InviteeItem.styles

--- src/components/InviteeItem/InviteeItem.js
+++ src/components/InviteeItem/InviteeItem.js
@@ -5,12 +5,11 @@ import Tick from 'assets/images/inviteeIcons/tick/default.png';
 import Cross from 'assets/images/inviteeIcons/cross/default.png';
 
 import BulletItem from 'components/BulletItem';
-import styles from './InviteeItem.styles';
 
 const InviteeItem = ({ id, name, confirmed, attend }) => {
   return (
-    <BulletItem style={styles.container} id={id} text={name}>
-      {confirmed && <Image style={styles.check} source={attend ? Tick : Cross} />}
+    <BulletItem id={id} text={name}>
+      {confirmed && <Image source={attend ? Tick : Cross} />}
     </BulletItem>
   );
 };
--- src/components/InviteeItem/InviteeItem.styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    marginLeft: 'auto',
-  },
-});
-
-export default styles;
