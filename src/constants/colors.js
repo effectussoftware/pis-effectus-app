9e6d379be1892af1c860e1e831edae8838d8764b
Agustina Chaer <aguschaer@gmail.com>
2020-11-09 20:42:19 -0300

    fix: order of colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,10 +3,11 @@ export const SECONDARY = '#4A6CCB';
 
 export const WHITE = '#FFFFFF';
 export const SUPER_LIGHT_GRAY = '#F6F6F6';
-export const BLUE = '#0000FF';
 export const LIGHT_GRAY = '#E8E8E8';
 export const GRAY = '#BDBDBD';
 export const DARK_GRAY = '#666666';
 export const BLACK = '#000000';
 
 export const ALERT = '#FF5252';
+
+export const BLUE = '#0000FF';
